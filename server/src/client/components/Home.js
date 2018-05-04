347dc2e12d59dc9643e2c91a8bbf57cdf1f98e45
iDatLe <LeDatDac@gmail.com>
2018-05-04 14:46:35 -0700

    Redux involved. Before Routes.js change

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return(
         <div>
-            <div>I am the only home component </div>
+            <div>I am the only home component. Dat Le.</div>
             <button onClick={() => console.log('Hi there!')}>Press me!</button>
         </div>
     );
--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,20 @@
 //THIS FILE IS THE EXPRESS LOGIC
 
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
  // Tells express to treat the public directory as a static
 app.use(express.static('public'));
 app.get('*', (req, res) => { // The 'req' contains the url the user is trying to access
-    res.send(renderer(req));
+    const store = createStore();
+
+    // Some logic to initialize and load data into store
+
+    res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
